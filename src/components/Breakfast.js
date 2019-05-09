1bc52a67cbb0df581bdc09dbe1b6a5f40be0c490
MitziYolotzin <ultravisuell@hotmail.com>
2019-05-09 16:18:57 -0500

    add images for breakfast, add one button, align adjust css navbar

--- src/components/Breakfast.js
+++ src/components/Breakfast.js
@@ -1,14 +1,16 @@
 import React  from 'react';
 import '../css/Breakfast.css';
 
+
 const Breakfast = () => {
     return(
-        
+      
 <div>
-<h1>Breakfast</h1>
+
+<h1>Breakfast</h1> 
     
 <div className="boxed">
-  This text is enclosed in a box.
+  Sandwich
 </div>
 
 </div>
